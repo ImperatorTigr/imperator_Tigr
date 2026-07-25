7370ae7a848c0eb6f7cc5644a2ce509018bf3714
ImperatorTigr <6231110@gmail.com>
2026-07-25 03:00:09 +0300

    L2: многофазная анимация лого

--- src/js/components/site-logo.js
+++ src/js/components/site-logo.js
@@ -30,17 +30,17 @@ class SiteLogo extends HTMLElement{
 <line class="logo-grid-line" x1="20"  y1="100" x2="280" y2="100"/>
 <line class="logo-grid-line" x1="20"  y1="200" x2="280" y2="200"/>
 
-<text class="logo-letter" x="50"  y="50">I</text>
-<text class="logo-letter" x="150" y="50">M</text>
-<text class="logo-letter" x="250" y="50">P</text>
+<text class="logo-letter" style="--tx:100px;--ty:100px"   x="50"  y="50">I</text>
+<text class="logo-letter" style="--tx:0px;--ty:100px"     x="150" y="50">M</text>
+<text class="logo-letter" style="--tx:-100px;--ty:100px"  x="250" y="50">P</text>
 
-<text class="logo-letter" x="50"  y="150">E</text>
-<text class="logo-letter" x="150" y="150">R</text>
-<text class="logo-letter" x="250" y="150">A</text>
+<text class="logo-letter" style="--tx:100px;--ty:0px"     x="50"  y="150">E</text>
+<text class="logo-letter" style="--tx:0px;--ty:0px"       x="150" y="150">R</text>
+<text class="logo-letter" style="--tx:-100px;--ty:0px"    x="250" y="150">A</text>
 
-<text class="logo-letter" x="50"  y="250">T</text>
-<text class="logo-letter" x="150" y="250">O</text>
-<text class="logo-letter" x="250" y="250">R</text>
+<text class="logo-letter" style="--tx:100px;--ty:-100px"  x="50"  y="250">T</text>
+<text class="logo-letter" style="--tx:0px;--ty:-100px"    x="150" y="250">O</text>
+<text class="logo-letter" style="--tx:-100px;--ty:-100px" x="250" y="250">R</text>
 
 <text class="logo-word" x="150" y="150">Tigr</text>
 
