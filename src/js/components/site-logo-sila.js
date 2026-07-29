feba83aa9f4d346585743167b476cecd15dba73d
ImperatorTigr <6231110@gmail.com>
2026-07-29 06:33:12 +0300

    fix: сломанный id градиента + пропорции флага и слова ИГРА

--- src/js/components/site-logo-sila.js
+++ src/js/components/site-logo-sila.js
@@ -1,20 +1,18 @@
-function buildFace(faceId,letters){
+function buildFace(letters,includeDefs){
 
-    const chip=(pos,cx,cy,rx,ry,ch,delay)=>`
+    const chip=(pos,x,y,ch,delay)=>`
 
-<rect class="sila-chip-rect sila-chip-${pos}" x="${cx}" y="${cy}" width="${rx}" height="${ry}" rx="18" style="--chip-delay:${delay}"/>
+<rect class="sila-chip-rect" x="${x}" y="${y}" width="130" height="130" rx="18" style="--chip-delay:${delay}"/>
 
-<text class="sila-chip-letter" x="${cx+rx/2}" y="${cy+ry/2+2}" style="--chip-delay:${delay}">${ch}</text>
+<text class="sila-chip-letter" x="${x+65}" y="${y+67}" style="--chip-delay:${delay}">${ch}</text>
 
 `;
 
-    return `
-
-<svg class="sila-svg" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+    const defs=includeDefs?`
 
 <defs>
 
-<linearGradient id="silaGold${faceId}" x1="0" y1="0" x2="1" y2="1">
+<linearGradient id="silaGold" x1="0" y1="0" x2="1" y2="1">
 <stop offset="0%" stop-color="var(--gold-light)"/>
 <stop offset="50%" stop-color="var(--gold-mid)"/>
 <stop offset="100%" stop-color="var(--gold-deep)"/>
@@ -22,10 +20,18 @@ function buildFace(faceId,letters){
 
 </defs>
 
-${chip("tl",10,10,130,130,letters.tl,"0s")}
-${chip("tr",160,10,130,130,letters.tr,".12s")}
-${chip("bl",10,160,130,130,letters.bl,".24s")}
-${chip("br",160,160,130,130,letters.br,".36s")}
+`:"";
+
+    return `
+
+<svg class="sila-svg" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">
+
+${defs}
+
+${chip("tl",10,10,letters.tl,"0s")}
+${chip("tr",160,10,letters.tr,".12s")}
+${chip("bl",10,160,letters.bl,".24s")}
+${chip("br",160,160,letters.br,".36s")}
 
 <path class="sila-hint" transform="translate(150,150)"
       d="M-4 -22 C-24 -22 -32 -6 -22 10 C-14 22 6 22 14 10"/>
@@ -35,27 +41,33 @@ ${chip("br",160,160,130,130,letters.br,".36s")}
 
 <g class="sila-flag-group">
 
-<rect class="sila-flag-shape" x="118" y="-22" width="64" height="26" rx="6"/>
+<rect class="sila-flag-shape" x="115" y="-30" width="70" height="24" rx="6"/>
 
-<rect class="sila-flag-shape" x="138" y="0" width="24" height="30" rx="4"/>
+<rect class="sila-flag-shape" x="137" y="-6" width="26" height="110" rx="5"/>
 
-<line class="sila-flag-stripe" x1="126" y1="-16" x2="140" y2="0"/>
+<line class="sila-flag-stripe" x1="122" y1="-24" x2="136" y2="-8"/>
 
-<line class="sila-flag-stripe" x1="140" y1="-16" x2="154" y2="0"/>
+<line class="sila-flag-stripe" x1="136" y1="-24" x2="150" y2="-8"/>
 
-<line class="sila-flag-stripe" x1="154" y1="-16" x2="168" y2="0"/>
+<line class="sila-flag-stripe" x1="150" y1="-24" x2="164" y2="-8"/>
 
-<line class="sila-flag-stripe" x1="144" y1="6" x2="156" y2="18"/>
+<line class="sila-flag-stripe" x1="141" y1="8" x2="155" y2="22"/>
+
+<line class="sila-flag-stripe" x1="141" y1="32" x2="155" y2="46"/>
+
+<line class="sila-flag-stripe" x1="141" y1="56" x2="155" y2="70"/>
+
+<line class="sila-flag-stripe" x1="141" y1="80" x2="155" y2="94"/>
 
 </g>
 
-<text class="sila-igra-letter" x="150" y="60">${letters.igra[0]}</text>
+<text class="sila-igra-letter" x="150" y="135">${letters.igra[0]}</text>
 
-<text class="sila-igra-letter" x="150" y="92">${letters.igra[1]}</text>
+<text class="sila-igra-letter" x="150" y="181">${letters.igra[1]}</text>
 
-<text class="sila-igra-letter" x="150" y="208">${letters.igra[2]}</text>
+<text class="sila-igra-letter" x="150" y="227">${letters.igra[2]}</text>
 
-<text class="sila-igra-letter" x="150" y="240">${letters.igra[3]}</text>
+<text class="sila-igra-letter" x="150" y="273">${letters.igra[3]}</text>
 
 </svg>
 
@@ -67,9 +79,9 @@ class SiteLogoSila extends HTMLElement{
 
     connectedCallback(){
 
-        const front=buildFace("Front",{tl:"С",tr:"А",bl:"И",br:"Л",igra:["И","Г","Р","А"]});
+        const front=buildFace({tl:"С",tr:"А",bl:"И",br:"Л",igra:["И","Г","Р","А"]},true);
 
-        const back=buildFace("Back",{tl:"S",tr:"A",bl:"I",br:"L",igra:["I","G","R","A"]});
+        const back=buildFace({tl:"S",tr:"A",bl:"I",br:"L",igra:["I","G","R","A"]},false);
 
         this.innerHTML=`
 
@@ -94,12 +106,6 @@ class SiteLogoSila extends HTMLElement{
         const link=this.querySelector(".sila-logo");
 
         link.setAttribute("href",home);
-
-        setTimeout(()=>{
-
-            link.classList.add("is-split");
-
-        },400);
 
         setTimeout(()=>{
 
